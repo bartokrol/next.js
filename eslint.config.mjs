fdf60533d914f5a4157a00541419f410ba0b59b6
Bartosz Król <barto.kro@gmail.com>
2026-03-01 21:00:13 +0100

    Eslint configuration, remove tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +9,25 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      'no-unused-vars': 'error',
+      '@stylistic/jsx-closing-bracket-location': 'error',
+      '@stylistic/jsx-curly-brace-presence': ['error', 'always'],
+      '@stylistic/jsx-tag-spacing': ['error', { 'beforeClosing': 'never' }],
+      '@stylistic/semi': 'error',
+      '@stylistic/quotes': ['error', 'single']
+    }
+  }
 ]);
 
 export default eslintConfig;
