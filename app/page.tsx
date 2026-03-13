8a6617c18a1a7cea2588d5ac432f8541400cd7d0
Bartosz Król <barto.kro@gmail.com>
2026-03-13 22:20:47 +0100

    3-create first draft of the carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+import React from 'react';
+import styles from './home.module.scss';
+
 export default function Home() {
 
     return (
-        <div />
+        <div className={styles.container}>
+            {'test'}
+            <div>
+                <ul className={styles.ul}>
+                    <li className={styles.li}>{'Item 1'}</li>
+                    <li className={styles.li}>{'Item 2'}</li>
+                    <li className={styles.li}>{'Item 3'}</li>
+                    <li className={styles.li}>{'Item 4'}</li>
+                    <li className={styles.li}>{'Item 5'}</li>
+                    <li className={styles.li}>{'Item 6'}</li>
+                    <li className={styles.li}>{'Item 7'}</li>
+                    <li className={styles.li}>{'Item 8'}</li>
+                </ul>
+            </div>
+        </div>
     );
 }
